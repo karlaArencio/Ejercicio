b892cc6059e0476d5c645f2fc023f414f49a1685
karla <karlaarencio@hotmail.com>
2024-09-18 16:38:36 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, string> diccionario= new Dictionary<string, string>();
+            diccionario.Add("hello my name is karla", "hola mi nombre es karla");
+            diccionario["see you later"] = "hasta luego";
+
+            Console.WriteLine(diccionario["hello my name is karla"]);
+            
+
+                if (diccionario.ContainsKey("adios"))
+                    Console.WriteLine(diccionario["adios"]);
+                char[] delimitador = { ',', ' ' };
+                string[] palabras = ("hola mi nombre es karla").Split(delimitador);
+                int i;
+                for (i=0;i<palabras.Length; i++)
+                {
+                   
+                    Console.WriteLine("fragmento{0}={1}", i + 1, palabras[i]);
+
+                }  
+
+         Console.ReadKey();
+
+        }
+    }
+        
+}
